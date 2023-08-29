8c8c6527cebafc8fbfe5273dd1e636582ce0d95b
Kris Templin <kris.templin@gmail.com>
2023-08-28 20:12:18 -0700

    Set form results to JSON output in console

--- scripts.js
+++ scripts.js
@@ -1,7 +1,20 @@
-const placeForResults = document.getElementById("results")
+//  ###Original script from Repo###
+//  const placeForResults = document.getElementById("results")
 
-new URLSearchParams(window.location.search).forEach((value, name) => {
-  placeForResults.append(`${name} : ${value}`)
-  placeForResults.append(document.createElement("br"))
-  
-})
+// new URLSearchParams(window.location.search).forEach((value, name) => {
+//   placeForResults.append(`${name} : ${value}`)
+//   placeForResults.append(document.createElement("br"))
+// })
+
+const form = document.querySelector("#formBox");
+
+form.addEventListener("submit", function(event) {
+  event.preventDefault(); 
+  let formData = new FormData(this);
+  let object = {};
+  formData.forEach((value, key) => {
+    object[key] = value
+  });
+  let json = JSON.stringify(object);
+  console.log(json);
+}, false);
