8135e973b6cb959023cba46d7e7d7d53de12473e
Kris Templin <kris.templin@gmail.com>
2023-08-28 20:37:20 -0700

    Fix a few bugs with flow.

--- scripts.js
+++ scripts.js
@@ -1,20 +1,22 @@
 //  ###Original script from Repo###
-//  const placeForResults = document.getElementById("results")
+const placeForResults = document.getElementById("results")
 
-// new URLSearchParams(window.location.search).forEach((value, name) => {
-//   placeForResults.append(`${name} : ${value}`)
-//   placeForResults.append(document.createElement("br"))
-// })
+new URLSearchParams(window.location.search).forEach((value, name) => {
+  placeForResults.append(`${name} : ${value}`)
+  placeForResults.append(document.createElement("br"))
+})
 
 const form = document.querySelector("#formBox");
 
 form.addEventListener("submit", function(event) {
-  event.preventDefault(); 
+  // *** Commented out because it stops me from going to results. 
+  //      Need to figure out what I need here instead ***
+  // event.preventDefault(); 
   let formData = new FormData(this);
   let object = {};
   formData.forEach((value, key) => {
     object[key] = value
   });
   let json = JSON.stringify(object);
-  console.log(json);
+  console.log(JSON.stringify(json, undefined, 2));
 }, false);
