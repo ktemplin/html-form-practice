4f3e1aef56036052444a0792f08a8b371eb2fa19
Kris Templin <kris.templin@gmail.com>
2023-08-28 19:30:38 -0700

    Changes to Contact for alignment with Portfolio design

--- scripts.js
+++ scripts.js
@@ -3,4 +3,5 @@ const placeForResults = document.getElementById("results")
 new URLSearchParams(window.location.search).forEach((value, name) => {
   placeForResults.append(`${name} : ${value}`)
   placeForResults.append(document.createElement("br"))
+  
 })
